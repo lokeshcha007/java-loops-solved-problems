3ee9859416ab0faabf821cdc2613961afc31b2b4
Lokeshch420 <lokeshcha2002@gmail.com>
2024-09-02 21:02:33 +0530

    Add program to find largest and smallest numbers entered by the user
    - Implemented logic to repeatedly enter numbers until user chooses to stop
    - Added functionality to track and display the largest and smallest numbers entered

---
+++ FindLargestAndSmallest.java
@@ -0,0 +1,37 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+package loops;
+
+import java.util.*;
+
+class HelloWorld {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        boolean UserDesire = true;
+        int num, max = Integer.MIN_VALUE, min = Integer.MAX_VALUE;
+        System.out.println(max);
+        System.out.println(min);
+        while (UserDesire) {
+            System.out.print("Enter your number1 : ");
+            int num1 = scn.nextInt();
+            System.out.print("Enter your number2 : ");
+            int num2 = scn.nextInt();
+            if (num1 > max) {
+                max = num1;
+                // System.out.println(num1);
+            }
+            if (num2 <= min) {
+                min = num2;
+            }
+            System.out.println("Do you want to enter another number (1/0)");
+            int choice = scn.nextInt();
+            if (choice == 0) {
+                break;
+            }
+
+        }
+        System.out.println(max);
+        System.out.println(min);
+
+    }
+}
