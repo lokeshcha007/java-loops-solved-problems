27a0925da58935f152f632e73d0d69bae848a70f
Lokeshch420 <lokeshcha2002@gmail.com>
2024-09-02 10:36:23 +0530

    alphabets from a-z and in reverse order also

---
+++ Alphabets.java
@@ -0,0 +1,13 @@
+package loops;
+
+public class Alphabets {
+    public static void main(String[] args) {
+        // for (int i = 'A'; i <= 'Z'; i++) {
+        // System.out.println((char) i);
+        // }
+
+        for (int i = 'Z'; i >= 'A'; i--) {
+            System.out.println((char) i);
+        }
+    }
+}
